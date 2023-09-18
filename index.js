bd2f60e61e80cea7312121cadfad7a65ad48fdd7
Banja David Lotigo <innocdavid@gmail.com>
2023-09-18 16:27:37 +0000

    modified - add database connection to index file

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ import morgan from 'morgan';
 import dotenv from 'dotenv';
 import colors from 'colors';
 
+import connectDB from './config/db.js';
+
 const app = express();
 
 dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
+
+// connect db
+connectDB();
 
 
 app.get('/', async(req, res) => {
