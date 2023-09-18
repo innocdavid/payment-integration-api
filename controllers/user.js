a764223ca4a4a8a073ab204bb1d7f0bc6b6f8b71
Banja David Lotigo <innocdavid@gmail.com>
2023-09-18 17:07:50 +0000

    feature - add controller to create a new user

---
+++ controllers/user.js
@@ -0,0 +1,24 @@
+// import modules
+import bcrypt from 'bcrypt';
+import { expressValidator } from 'express-validator';
+import colors from 'colors';
+import User from '../models/user.js';
+
+const createUser = async (req, res) => {
+    const errors = expressValidator(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password, firstName, lastName } = req.body;
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ email, password: hashedPassword, firstName, lastName });
+        await user.save();
+        res.status(201).json({ data: user, message: 'User created successfully' });
+    } catch (err) {
+        console.error(err.bgRed);
+        res.status(500).json({ error: err.message });
+    }
+}
